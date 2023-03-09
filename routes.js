6c69904130f0e9a3d51319871ec44fc83e280c96
Shirley Startary <shirley.startary@gmail.com>
2023-03-09 13:45:55 -0600

    Add route to delete products

--- routes.js
+++ routes.js
@@ -141,4 +141,23 @@ router.put('/products/:id', bodyParser, (req, res) => {
     })
 })
 
+router.delete('/products/:id', (req, res) => {
+    elasticClient.delete({
+        index:'products',
+        id: req.params.id
+    })
+    .then( resp => {
+        res.status(200).json({
+            msg:'Product deleted',
+            resp
+        })
+    })
+    .catch(err => {
+        res.status(500).json({
+            msg: 'Error',
+            err
+        })
+    })
+})
+
 module.exports = router;
